eb159565f33fa1d96fe2c208bfbfccd9b4f46323
Ionut Spataru <ionut.spataru@consultant.LEGO.com>
2019-02-26 20:19:23 +0200

    Add project files.

---
+++ CacheTheCow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CacheTheCow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
